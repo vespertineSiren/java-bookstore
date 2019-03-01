1dec128c0171307f80965751803974548ccd506d
Patrick Martin <patrickmartin88@gmail.com>
2019-03-01 08:23:43 -0800

    Initial Commit

---
+++ src/main/java/dev/vespertine/javabookstore/JavabookstoreApplication.java
@@ -0,0 +1,13 @@
+package dev.vespertine.javabookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavabookstoreApplication.class, args);
+    }
+
+}

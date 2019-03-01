e79ccca461a0328a8ca3dbdfe57a1ddf8f14ba20
Patrick Martin <patrickmartin88@gmail.com>
2019-03-01 09:31:35 -0800

    Added completed models.

---
+++ src/main/java/dev/vespertine/javabookstore/Models/Author.java
@@ -0,0 +1,54 @@
+package dev.vespertine.javabookstore.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long authorid;
+
+    private String lastname, firstname;
+
+    @ManyToMany(mappedBy = "authors")
+    @JsonIgnoreProperties("authors")
+    private Set<Book> books = new HashSet<>();
+
+    public long getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(long authorid) {
+        this.authorid = authorid;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
---
+++ src/main/java/dev/vespertine/javabookstore/Models/Book.java
@@ -0,0 +1,82 @@
+package dev.vespertine.javabookstore.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookid;
+
+    private String booktitle, isbn;
+
+    private Integer copy;
+
+    @ManyToMany
+    @JoinTable(name = "wrote",
+            joinColumns = {@JoinColumn(name = "bookid")},
+            inverseJoinColumns = {@JoinColumn(name = "authorid")})
+    @JsonIgnoreProperties("authors")
+    private Set<Author> authors = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "section")
+    @JsonIgnoreProperties("books")
+    private Section section;
+
+    public long getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(long bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getCopy() {
+        return copy;
+    }
+
+    public void setCopy(Integer copy) {
+        this.copy = copy;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+}
+
+

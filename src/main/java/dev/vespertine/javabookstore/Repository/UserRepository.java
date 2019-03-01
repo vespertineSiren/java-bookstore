4c9d8496ecad8fa20dd1eb09198b6d0cc4344ecf
Patrick Martin <patrickmartin88@gmail.com>
2019-03-01 12:51:15 -0800

    added config files for authentication.

--- src/main/java/dev/vespertine/javabookstore/Repository/UserRepository.java
+++ src/main/java/dev/vespertine/javabookstore/Repository/UserRepository.java
@@ -4,6 +4,6 @@ import dev.vespertine.javabookstore.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
     User findByUsername(String username);
 }
